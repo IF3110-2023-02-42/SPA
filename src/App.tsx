3d49f7eb6bdadbbb8fa47098915b8497d6c8ac69
fadhilAmri <fadhil133113@gmail.com>
2023-11-08 07:21:11 +0700

    feat: add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
-import { RouterProvider } from "react-router";
-import { router } from "./route";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
+
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "login",
+    element: <Login />
+  },
+  {
+    path: "register",
+    element: <Register />
+  }
+]) 
+
 
 const App = () => {
-  return <RouterProvider router={router()} />;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
