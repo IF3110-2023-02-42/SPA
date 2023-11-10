40b558b95a4a0199fda82d97166e27b1c9a65d0b
fadhilAmri <fadhil133113@gmail.com>
2023-11-10 18:21:39 +0700

    feat: add toggleModal for addNewDiscussion

--- src/pages/discussion/AddNewDiscussion.tsx
+++ src/pages/discussion/AddNewDiscussion.tsx
@@ -1,19 +1,37 @@
 import { useState, ChangeEvent } from 'react';
 import { IoMdClose } from 'react-icons/io';
+interface AddNewDiscussionProps {
+    toggleModal: () => void;
+  }
 
-export default function AddNewDiscussion() {
+export default function AddNewDiscussion({toggleModal}: AddNewDiscussionProps) {
     const [uraianInput, setUraianInput] = useState<string>('Type Something');
 
     const handleUraianChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
       setUraianInput(event.target.value);
     };
+
+    const handleOverlayClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        if (event.target === event.currentTarget) {
+          toggleModal();
+        }
+      };
+    
+    const subumitNewDiscussion = ()=>{
+        // Add new discussion to REST
+
+        toggleModal();
+    }
+    
+
     return (
-        <div className="fixed top-0 z-30 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50">
+        <div className="fixed top-0 z-30 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 " onClick={handleOverlayClick} >
             <div className="bg-white flex flex-col items-center shadow-md rounded-md p-5 w-3/5">
                 <div className="flex justify-end w-full">
                     <IoMdClose
                         className="text-gray-400 hover:opacity-80 hover:cursor-pointer bg-white  rounded-full p-1"
                         size={30}
+                        onClick={toggleModal}
                     />
                 </div>
                 <h3 className="font-bold text-2xl"> Buat Diskusi Baru </h3>
@@ -35,7 +53,7 @@ export default function AddNewDiscussion() {
                     </div>
 
                     <div className='flex justify-end w-full'>
-                        <p className='bg-purpleBg text-white p-2 rounded-md hover:cursor-pointer hover:opacity-90'>
+                        <p className='bg-purpleBg text-white p-2 rounded-md hover:cursor-pointer hover:opacity-90' onClick={subumitNewDiscussion}>
                             Buat Diskusi Baru
                         </p>
                     </div>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,17 +1,24 @@
 import Navbar from "../../components/Navbar";
 import AddDiscussion from "../discussion/AddNewDiscussion";
 import { FaPlus, FaComment} from 'react-icons/fa';
+import { useState} from 'react';
 
 const Home = () => {
+  const [modal, setModal] = useState(false);
+  const toggleModal = () => {
+    setModal(!modal);
+    console.log(modal);
+  };
+
   return (
     <div className="bg-creamBg z-10 min-h-screen">
       <Navbar />
-      <AddDiscussion/>
-      <div className="  mx-auto px-2 sm:px-6 lg:px-8  relative">
+      {modal && <AddDiscussion toggleModal={toggleModal}/>}
+      <div className=" mx-auto px-2 sm:px-6 lg:px-8  relative">
         <div className="w-full relative flex flex-col items-center justify-center py-5">
           <h1 className="text-4xl font-bold">Diskusi</h1>
-          <div className="pb-5"> 
-            <div className="mt-5 p-5 shadow-md rounded-3xl transition duration-300 ease-in-out hover:cursor-pointer hover:scale-105 bg-white">
+          <div className="w-full pb-5 flex flex-col items-center"> 
+            <div className="w-3/5 mt-5 p-5 shadow-md rounded-3xl transition duration-300 ease-in-out hover:cursor-pointer hover:scale-105 bg-white">
                 <div className="pb-3">
                   <div className="flex space-x-4 items-center">
                       <h2 className="text-2xl font-bold">Judul Diskusi</h2>
@@ -21,6 +28,7 @@ const Home = () => {
                 </div>
                 <div className="pb-3">
                     economic bubble adalah suatu peristiwa saat harga sebuah sekuritas berada jauh di atas harga wajarnya.
+
                 </div>
                 <div className="discussion-numOfComment flex items-center space-x-4">
                     <FaComment className="text-gray-300 rounded-full"  size={45}/>
@@ -31,7 +39,11 @@ const Home = () => {
           </div>
         </div>
         <div className="fixed w-full py-10 pr-20 flex justify-end bottom-0 z-20">
-          <FaPlus className="text-creamBg shadow-md bg-purpleBg opacity-100 transition duration-300 ease-in-out hover:scale-105 hover:cursor-pointer mr-2 p-1.5 rounded-full"  size={70}/>
+          <FaPlus 
+            className="text-creamBg shadow-md bg-purpleBg opacity-100 transition duration-300 ease-in-out hover:scale-105 hover:cursor-pointer mr-2 p-1.5 rounded-full"  
+            size={70}
+            onClick = {toggleModal}
+          />
         </div>
       </div>
     </div>
